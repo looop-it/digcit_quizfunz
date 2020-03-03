2e677598c307c0e3efa51cc56162f94fab3d57db
Vinson YUNG <vinson.yung@looop.hk>
2020-03-03 14:13:58 +0800

    Added mobile game page

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -24,6 +24,9 @@ window.Swal = require('sweetalert2');
 Vue.component('competition', require('./components/competition/Start.vue'));
 Vue.component('competition-result', require('./components/competition/Result.vue'));
 
+Vue.component('competition-mobile', require('./components/competition/mobile/Start.vue'));
+Vue.component('competition-moible-result', require('./components/competition/mobile/Result.vue'));
+
 const app = new Vue({
     el: '#app'
 });
