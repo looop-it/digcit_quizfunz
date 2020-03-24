d9136cbc5d5204a3f40fdc01e7357a0f7e2468c5
Vinson YUNG <vinson.yung@looop.hk>
2020-03-24 17:58:27 +0800

    Added job to send email to not yet played user

---
+++ app/Jobs/SendEmailToNotYetPlayedUsers.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\InvitePlayingGame;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmailToNotYetPlayedUsers implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        User::where('source', 'shi-online')
+            ->chunk(500, function ($users) {
+                foreach ($users as $user) {
+                    if ($user->papers->count() == 0) {
+                        Mail::to($user->email)->queue(new InvitePlayingGame());
+                    }
+                }
+                
+                sleep(5);
+            });
+    }
+}
---
+++ app/Mail/InvitePlayingGame.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class InvitePlayingGame extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subject;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        $this->subject = '立即參加「歷史在線」挑戰賽贏取$200現金券';
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject($this->subject)
+                    ->view('emails.invite_playing_game');
+    }
+}
