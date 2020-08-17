152af7b243c5aeb18f4df053e83f6b562f394ef6
Vinson YUNG <vinson.yung@looop.hk>
2020-08-17 19:08:18 +0800

    Foced to generate https in assets path in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+
         \Laravel\Horizon\Horizon::auth(function () {
             // use Laravel-Admin Auth for Horizon authorizetion
             return (Admin::user()) ? true : false;
