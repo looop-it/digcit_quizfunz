9233129f959e98a4abdecc0515f8c5bab79a7b69
Vinson YUNG <vinson.yung@looop.hk>
2020-10-05 18:58:43 +0800

    Added return to list button

--- resources/views/news_detail.blade.php
+++ resources/views/news_detail.blade.php
@@ -19,7 +19,13 @@
 							</div>
 						</div>
 					</div>
-                </div>
+				</div>
+				
+				<div class="row mt-3">
+					<div class="col-md-12 text-center">
+						<a href="{{ route('references') }}" class="btn btn-main">返回列表</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
