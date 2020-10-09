0764899d210bda85ec0168a1c01c19e56723c6e2
Vinson YUNG <vinson.yung@looop.hk>
2020-10-09 11:33:37 +0800

    FIxed footer issue

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,14 +1,14 @@
-<div class="container">
+<div class="container footer">
 	<div class="row">
 		<div class="col-md-12 col-sm-12">
-			<div class="footer text-center">
+			<div class="sponsors text-center">
 				<div class="row visible-md-block visible-lg-block">
-					<div class="col-md-12">
+					<div class="col-md-12">	
 						主辦機構：
 						<img src="/images/logo/youthinkers.png" class="img-fluid">
 						<img src="/images/logo/shinetak.png" class="img-fluid">
 
-						合作媒體：
+						協辦機構：
 						<img src="/images/logo/logo_1.png" class="img-fluid">
 						<img src="/images/logo/logo_2.png" class="img-fluid">
 						<img src="/images/logo/logo_3.png" class="img-fluid">
@@ -28,32 +28,41 @@
 						<img src="/images/logo/logo_3.png" class="img-fluid">
 					</div>
 				</div>
+			</div>
+		</div>
+	</div>
 
-				<div class="row links">
-					<div class="col-md-12">
-						<ul class="horizontal">
-							<li>
-								<a href="{{ route('page.detail', ['slug' => '私隱聲明']) }}">{{trans('home.footer.privacy')}}</a>
-								<div class="line"></div>
-							</li>
-							<li>
-								<a href="{{ route('page.detail', ['slug' => '免責條款']) }}">{{trans('home.footer.disclaimer')}}</a>
-								<div class="line"></div>
-							</li>
-							<li>
-								<a href="{{ route('page.detail', ['slug' => '關於我們']) }}">{{trans('home.footer.about_us')}}</a>
-								<div class="line"></div>
-							</li>
-							<li>
-								<a href="{{ route('enquiry') }}">{{trans('home.footer.contact_us')}}</a>
-							</li>
-						</ul>
-					</div>
-				</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="links">
+				<ul class="horizontal">
+					<li>
+						<a
+							href="{{ route('page.detail', ['slug' => '私隱聲明']) }}">{{trans('home.footer.privacy')}}</a>
+						<div class="line"></div>
+					</li>
+					<li>
+						<a
+							href="{{ route('page.detail', ['slug' => '免責條款']) }}">{{trans('home.footer.disclaimer')}}</a>
+						<div class="line"></div>
+					</li>
+					<li>
+						<a
+							href="{{ route('page.detail', ['slug' => '關於我們']) }}">{{trans('home.footer.about_us')}}</a>
+						<div class="line"></div>
+					</li>
+					<li>
+						<a href="{{ route('enquiry') }}">{{trans('home.footer.contact_us')}}</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 
-				<div class="copyright">
-					青識教育發展中心版權所有 Copyright &copy; 2020 Youthinkers ltd. All rights reserved.
-				</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="copyright">
+				青識教育發展中心版權所有 Copyright &copy; 2020 Youthinkers ltd. All rights reserved.
 			</div>
 		</div>
 	</div>
