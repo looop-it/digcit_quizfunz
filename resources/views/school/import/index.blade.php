f559ec1cac8424ff54ce2d9e39a75d1c3598d50c
Vinson YUNG <vinson.yung@looop.hk>
2020-10-09 18:26:28 +0800

    Updated teacher login form

--- resources/views/school/import/index.blade.php
+++ resources/views/school/import/index.blade.php
@@ -12,7 +12,7 @@
 
                             <form class="form-horizontal" method="post" action="{{ route('student_account_import.login') }}">
                                 {{ csrf_field() }}
-                                <div class="form-group">
+                                <div class="form-group @if($errors->has('email')) has-error @endif">
                                     <label for="email" class="col-sm-3 control-label">電郵地址</label>
                                     <div class="col-sm-9">
                                         <input type="email" class="form-control" id="email" name="email" placeholder="請輸入電郵地址" value="{{ $data ? $data['email'] : old('email') }}">
@@ -24,7 +24,7 @@
                                         @endif
                                     </div>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group @if($errors->has('token')) has-error @endif">
                                     <label for="token" class="col-sm-3 control-label">驗證碼</label>
                                     <div class="col-sm-9">
                                         <input type="password" class="form-control" id="token" name="token" placeholder="請輸入驗證碼" value="{{ $data ? $data['token'] : '' }}">
