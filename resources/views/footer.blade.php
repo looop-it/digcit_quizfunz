52506d283d13244084b4c6884608a42ae7ff8b30
Vinson YUNG <vinson.yung@looop.hk>
2020-10-09 15:12:15 +0800

    Updated footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,22 +3,49 @@
 		<div class="col-md-12 col-sm-12">
 			<div class="sponsors text-center">
 				<div class="row visible-md-block visible-lg-block">
-					<div class="col-md-12">	
-						主辦機構：
-						<img src="/images/logo/youthinkers.png" class="img-fluid">
-						<img src="/images/logo/shinetak.png" class="img-fluid">
+					<div class="col-md-offset-1 col-md-5">
+						<div class="row">
+							<div class="col-md-12 text-left">主辦機構：</div>
+						</div>
 
-						協辦機構：
-						<img src="/images/logo/logo_1.png" class="img-fluid">
-						<img src="/images/logo/logo_2.png" class="img-fluid">
-						<img src="/images/logo/logo_3.png" class="img-fluid">
+						<div class="row">
+							<div class="col-md-6 col-xs-6">
+								<img src="/images/logo/youthinkers.png" class="img-fluid">
+							</div>
+		
+							<div class="col-md-6 col-xs-6">
+								<img src="/images/logo/shinetak.png" class="img-fluid">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-5">
+						<div class="row">
+							<div class="col-md-12 text-left">協辦機構：</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-4">
+								<img src="/images/logo/logo_1.png" class="img-fluid">
+							</div>
+
+							<div class="col-md-4">
+								<img src="/images/logo/logo_2.png" class="img-fluid">
+							</div>
+
+							<div class="col-md-4">
+								<img src="/images/logo/logo_3.png" class="img-fluid">
+							</div>
+						</div>
 					</div>
 				</div>
 
 				<div class="row visible-xs">
 					<div class="col-xs-12">主辦機構：</div>
-					<div class="col-xs-12">
+					<div class="col-xs-6">
 						<img src="/images/logo/youthinkers.png" class="img-fluid">
+					</div>
+					<div class="col-xs-6">
 						<img src="/images/logo/shinetak.png" class="img-fluid">
 					</div>
 					<div class="col-xs-12">協辦機構：</div>
