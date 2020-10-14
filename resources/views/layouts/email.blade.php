055e09ccbec551af8298167e4d6c40cf64f5ee79
Vinson YUNG <vinson.yung@looop.hk>
2020-10-14 16:28:22 +0800

    Fixed email layout issue

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -28,8 +28,8 @@
     </thead>
     <tbody>
         <tr>
-            <td style="padding:40px; font-size:1rem; line-height: 1.4rem;">
-                <div style="margin:0 auto; width:80%; padding:20px; -webkit-box-sizing:border-box; box-sizing:border-box;">
+            <td style="font-size:1rem; line-height: 1.4rem;">
+                <div style="margin:0 auto; width:80%; -webkit-box-sizing:border-box; box-sizing:border-box;">
                     <div>
                         @yield('content')
                     </div>
