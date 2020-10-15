88f01e6f75bbd10ba5446147273c986f48bcb3e3
Vinson YUNG <vinson.yung@looop.hk>
2020-10-15 19:26:04 +0800

    Enhanced constructor logic

--- app/Traits/HasCacheTrait.php
+++ app/Traits/HasCacheTrait.php
@@ -8,15 +8,17 @@ trait HasCacheTrait
 {
     public function __construct()
     {
+        parent::__construct();
+
         $this->clearCache();
     }
 
     public function clearCache()
     {
-        if (!property_exists(__CLASS__, 'cacheKey') || $this->cacheKey == null) {
-            throw new \Exception("property cacheKey is not defined in class " . __CLASS__);
+        if (property_exists(__CLASS__, 'cacheKey') && $this->cacheKey == null) {
+            Cache::forget($this->cacheKey);
         }
-
-        Cache::forget($this->cacheKey);
+        
+        throw new \Exception("property cacheKey is not defined in class " . __CLASS__);
     }
 }
