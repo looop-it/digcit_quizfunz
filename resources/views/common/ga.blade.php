33b006c5d429584d52c1a5b819f0cf489b4b24e6
Vinson YUNG <vinson.yung@looop.hk>
2021-03-19 16:10:32 +0800

    Updated Google Analytics

--- resources/views/common/ga.blade.php
+++ resources/views/common/ga.blade.php
@@ -7,3 +7,13 @@
 
   gtag('config', 'G-KZBNHHXY1M');
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-192608591-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-192608591-1');
+</script>
