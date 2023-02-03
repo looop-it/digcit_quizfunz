a2ef41986287e59dbfc9d6a7f60d7a83bc212c4f
YK.Yau <556092@qq.com>
2023-02-03 15:03:16 +0800

    update school daily report email template

--- resources/views/emails/report/school_daily_report.blade.php
+++ resources/views/emails/report/school_daily_report.blade.php
@@ -10,10 +10,10 @@
             <p>截止今早8時， 貴校學生作賽報告如下。</p>
 
             <p>
-                全校人數：{{ $school->student }}<br />
-                預計參加人數：{{ $school->expected_participant }}<br />
-                已作賽人數：{{ $school->actual_participant }}<br />
-                貴校參與率：{{ round($school->actual_participant / $school->student * 100, 2) }}%
+                全校人數：{{ $school->student }} (以學校登記時提交的人數為參考)<br />
+                {{-- 預計參加人數：{{ $school->expected_participant }}<br /> --}}
+                已作賽人數：{{ $school->statistics()->first()->participants ? $school->statistics()->first()->participants : 0 }}<br />
+                {{-- 貴校參與率：{{ round($school->actual_participant / $school->student * 100, 2) }}% --}}
             </p>
             <p>
                 隨電郵附上已報名學生詳細資料，歡迎查閱。如有任何查詢，請<a href="{{ route('enquiry') }}" target="blank">聯絡我們</a>，謝謝！
