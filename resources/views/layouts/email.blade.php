bde8e4225c1251069ee0a6d25560aa70c9a1441c
YK.Yau <556092@qq.com>
2023-11-03 16:59:59 +0800

    update email template

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -21,7 +21,7 @@
         <tr>
             <th style="background: #284299; color: #FFF; padding:10px 20px;">
                 <div style="margin:0 auto; width:100%; max-width:600px; padding:10px; -webkit-box-sizing:border-box; box-sizing:border-box;">
-                    家國公民智多 FUN
+                    家國公民智多 FUN 全港中學生知識競賽
                 </div>
             </th>
         </tr>
